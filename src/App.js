1e98d430f36b634efd6ca64513a4165dc31f0595
CMCCVR <99026597+CMCCVR@users.noreply.github.com>
2022-08-16 20:42:39 -0300

    Inclusão de colunas

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import { FormBook } from "./components/FormBook/FormBook";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Table } from "./components/Table/Table";
 
-
 function App() {
   return (
     <div className="container">
-
       <NavBar />
       <FormBook />
       <Table />
-
     </div>
   );
 }
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,45 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
-export const Table = () =>{
-    let [posts, setPosts] = useState([])
+export const Table = () => {
+    // 1º criar o state que vai conter a lista da API
+    let [Comments, setComments] = useState([])
+    // 2º criar o objeto colunas no qual o dataFild é a propriedade do objeto
     const columns = [
         {
             dataField: 'id',
             text: 'id'
-        },
-        {
-            dataField: 'title',
-            text: 'Titulo'
-        },
-        {
+        }, {
+            dataField: 'Nome',
+            text: 'Nome'
+        }, {
+            dataField: 'E-mail',
+            text: 'E-mail'
+        }, {
             dataField: 'body',
-            text: 'texto'
+            text: 'Texto'
         }
     ]
-
-    const getPosts = () => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+    const getComments = () => {
+        fetch('https://jsonplaceholder.typicode.com/Comments')
             .then(response => response.json())
             .then(data => {
                 // consol.log('posts', data)
-                setPosts(data)
+                setComments(data)
             })
     }
     useEffect(() => {
-        getPosts()
-
+        getComments()
     }, []);
-
     return (
-
         <div>
             <h3>Tabela de Usuários</h3>
             <BootstrapTable
                 keyField='id'
-                data={posts}
+                data={Comments}
                 columns={columns}
-
+                pagination={ paginationFactory() }
             />
         </div>
     )
